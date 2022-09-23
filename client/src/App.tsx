d56eaa1f2a882e689819b56c84ba35108c8aa5e2
usama-mir <97845216+usama-mir@users.noreply.github.com>
2022-09-23 09:05:51 +0200

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -20,7 +20,7 @@ const App: FC = () => {
                         exact path="/cardata" 
                         component={CarData} />
                     <Route 
-                        exact path="/roadie" 
+                        exact path="/roadies" 
                         component={RoadMeasurements} />
                     <Route 
                         exact path="/road_conditions"    
